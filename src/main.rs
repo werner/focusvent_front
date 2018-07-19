373f2b2bdee1f5beac6f9061d8f9a909d992729c
werner <werner@yahoo.es>
2018-07-19 17:47:48 -0300

    Add Products list route

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,50 @@ mod agents;
 mod services;
 
 use yew::prelude::*;
-use agents::router::Route;
+use agents::router;
 
-enum Msg {
-    HandleRoute(Route<()>)
+pub enum EComponent {
+    ProductList
 }
 
-struct Model {  }
+enum Msg {
+    NavigateTo(EComponent),
+    HandleRoute(router::Route<()>)
+}
+
+struct Model {
+    router: Box<Bridge<router::Router<()>>>
+}
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Model {  }
+        let callback = link.send_back(|route: router::Route<()>| Msg::HandleRoute(route));
+        let mut router = router::Router::bridge(callback);
+
+        router.send(router::Request::GetCurrentRoute);
+        Model { router }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::HandleRoute(route) => {
+                true
+            },
+            Msg::NavigateTo(component) => {
+                let route = router::Route {
+                    path_segments: vec!["products".to_string()],
+                    query: None,
+                    fragment: None,
+                    state: ()
+                };
 
+                self.router.send(router::Request::ChangeRoute(route));
+                false
             }
-        };
-        true
+        }
     }
 }
 
@@ -38,6 +59,8 @@ impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div>
+                <h1>{{ "Hola Mundo" }}</h1>
+                <button onclick=|_| Msg::NavigateTo(EComponent::ProductList),>{ "Lista de Productos" }</button>
             </div>
         }
     }
