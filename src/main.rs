19cbad2b1ccf38efbcd3e8d4cd86a10dab53913c
werner <werner@yahoo.es>
2018-07-19 15:53:04 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+extern crate stdweb;
+extern crate yew;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate log;
+
+mod agents;
+mod services;
+
+fn main() {
+    println!("Hello, world!");
+}
