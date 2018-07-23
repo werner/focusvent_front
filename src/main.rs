4437857fe043f6e060d6aa073b616643dc427ed7
werner <werner@yahoo.es>
2018-07-23 15:25:12 -0300

    Implements with trait object

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,72 @@ mod services;
 
 use yew::prelude::*;
 use agents::router;
+use std::collections::HashMap;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum ChildComponent {
-    ProductList,
-    Parent,
-    PathNotFound(String)
+#[derive(Clone)]
+struct ProductComponent {}
+
+impl ChildComponent for ProductComponent {
+    fn route(&self) -> String {
+        "products".to_string()
+    }
+
+    fn box_clone(&self) -> Box<ChildComponent> {
+        Box::new((*self).clone())
+    }
+}
+
+impl Component for ProductComponent {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        ProductComponent {  }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        true
+    }
+}
+
+impl Renderable<ProductComponent> for ProductComponent {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <div>
+                <h2> { "Productos" } </h2>
+            </div>
+        }
+    }
+}
+
+pub trait ChildComponent {
+    fn route(&self) -> String;
+    fn box_clone(&self) -> Box<ChildComponent>;
+}
+
+impl Clone for Box<ChildComponent>
+{
+    fn clone(&self) -> Box<ChildComponent> {
+        self.box_clone()
+    }
+}
+
+impl PartialEq for Box<ChildComponent> {
+    fn eq(&self, other: &Box<ChildComponent>) -> bool {
+        self.route() == other.route()
+    }
 }
 
 enum Msg {
-    NavigateTo(ChildComponent),
-    HandleRoute(router::Route<()>)
+    NavigateTo(Box<ChildComponent>),
+    HandleRoute(router::Route<()>),
+    AddRoute(String, Box<ChildComponent>)
 }
 
 struct RootModel {
-    child_component: ChildComponent,
-    router: Box<Bridge<router::Router<()>>>
+    child_component: Option<Box<ChildComponent>>,
+    router: Box<Bridge<router::Router<()>>>,
+    routes: HashMap<String, Box<ChildComponent>>
 }
 
 impl Component for RootModel {
@@ -36,33 +86,37 @@ impl Component for RootModel {
         let mut router = router::Router::bridge(callback);
 
         router.send(router::Request::GetCurrentRoute);
-        RootModel {
-            child_component: ChildComponent::Parent,
-            router 
-        }
+        RootModel { child_component: None, router, routes: HashMap::new() }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
+            Msg::AddRoute(path, component) => {
+                self.routes.insert(path, component);
+                false
+            },
             Msg::HandleRoute(route) => {
-                self.child_component = if let Some(first_segment) = route.path_segments.get(0) {
-                    match first_segment.as_str() {
-                        "products" => ChildComponent::ProductList,
-                        other => ChildComponent::PathNotFound(other.into())
-                    }
-                } else {
-                    ChildComponent::PathNotFound("path not found".into())
-                };
+                self.child_component = route.path_segments.get(0).and_then(|first_segment| {
+                    self.routes.get(first_segment.as_str()).and_then(|component| Some((*component).clone()))
+                }); 
+                    
                 true
             },
             Msg::NavigateTo(child_component) => {
+                let mut path_routes = Vec::new();
+                for (key_route, component) in &self.routes {
+                    if component == &child_component {
+                        path_routes.push(key_route.to_string());
+                        break;
+                    }
+                }
+
                 let route = router::Route {
-                    path_segments: vec!["products".to_string()],
+                    path_segments: path_routes, //TODO: Seems to required much more
                     query: None,
                     fragment: None,
                     state: ()
                 };
-
                 self.router.send(router::Request::ChangeRoute(route));
                 false
             }
@@ -75,37 +129,47 @@ impl Renderable<RootModel> for RootModel {
         html! {
             <div>
                 <h1>{ "Hola Mundo" }</h1>
-                <button onclick=|_| Msg::NavigateTo(ChildComponent::ProductList),>{ "Lista de Productos" }</button>
+                <button onclick=|_| Msg::NavigateTo(Box::new(ProductComponent {  })),>
+                    { "Lista de Productos" }
+                </button>
                 <div>
-                  { self.child_component.view() }
+                  { 
+                      if let Some(ref component) = self.child_component {
+                          component.view()
+                      } else {
+                          "".to_string()
+                      }
+                  }
                 </div>
             </div>
         }
     }
 }
 
-impl Renderable<RootModel> for ChildComponent {
-    fn view(&self) -> Html<RootModel> {
-        match *self {
-            ChildComponent::ProductList => {
-                html! {
-                    <h2> { "Lista de Productos" } </h2>
-                }
-            },
-            ChildComponent::Parent => {
-                html! { <div></div> }
-            },
-            ChildComponent::PathNotFound(ref path) => html! {
-                <div>
-                    {format!("Invalid path: '{}'", path)}
-                </div>
-            }
-        }
-    }
-}
+//impl Renderable<RootModel> for ChildComponent {
+//    fn view(&self) -> Html<RootModel> {
+//        match *self {
+//
+//            ChildComponent::ProductList => {
+//                html! {
+//                    <h2> { "Lista de Productos" } </h2>
+//                }
+//            },
+//            ChildComponent::Parent => {
+//                html! { <div></div> }
+//            },
+//            ChildComponent::PathNotFound(ref path) => html! {
+//                <div>
+//                    {format!("Invalid path: '{}'", path)}
+//                </div>
+//            }
+//        }
+//    }
+//}
 
 fn main() {
     yew::initialize();
-    App::<RootModel>::new().mount_to_body();
+    let mut app = App::<RootModel>::new().mount_to_body();
+    app.send_message(Msg::AddRoute("products".to_string(), Box::new(ProductComponent {  })));
     yew::run_loop();
 }
